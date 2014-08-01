28dcc36aa90cdef59939ee8d418ecdc1aa060070
Marc Rene Arns <arns@stridor.net>
2014-08-01 18:44:50 +0200

    improve documentation

--- wrap.go
+++ wrap.go
@@ -4,19 +4,22 @@ import "net/http"
 
 // Wrapper can wrap a http.Handler with another one
 type Wrapper interface {
-	// Wrap wraps the next `http.Handler` of the stack and returns the previous `http.Handler`
-	// If `next.ServeHTTP` is not called, the next `http.Handler` won't be used
-	Wrap(next http.Handler) (previous http.Handler)
+	// Wrap wraps the next `http.Handler` of the stack and returns a wrapping `http.Handler`.
+	// If it does not call `next.ServeHTTP`, nobody will.
+	Wrap(next http.Handler) http.Handler
 }
 
-// NoOp is a http.Handler that does nothing
-var NoOp = http.HandlerFunc(func(http.ResponseWriter, *http.Request) {})
-
-// New returns a http.Handler that wraps the given wrappers.
-// When it serves the request the first given wrapper
-// serves the request and may let the second wrapper (its "next" wrapper) serve.
-// The second wrapper may let the third wrapper serve and so on.
-// The last wrapper has as "next" wrapper the NoOp handler that does nothing.
+// New returns the wrapping http.Handler that returned from calling
+// the Wrap method on the first given wrapper that received the returned
+// handler of returning from the second wrappers Wrap method and so on.
+//
+// The last wrapper begins the loop receiving the NoOp handler.
+//
+// When the ServeHTTP method of the returned handler is called each wrapping
+// handler may call is next until the NoOp handler is run.
+//
+// Or some wrapper decides not to call next.ServeHTTP.
+//
 // If DEBUG is set, each handler is wrapped with a Debug struct that calls DEBUGGER.Debug before
 // running the handler.
 func New(wrapper ...Wrapper) (h http.Handler) {
@@ -35,3 +38,6 @@ type WrapperFunc func(http.Handler) http.Handler
 
 // Wrap makes the WrapperFunc fulfill the Wrapper interface by calling itself.
 func (wf WrapperFunc) Wrap(next http.Handler) http.Handler { return wf(next) }
+
+// NoOp is a http.Handler doing nothing
+var NoOp = http.HandlerFunc(func(http.ResponseWriter, *http.Request) {})
